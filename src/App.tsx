fc8f3ef69d32b3f460ebdda4c1b04e92adfdaa32
suminllll <krlatnalsl01@gmail.com>
2024-04-02 23:01:58 +0900

    스코어 분석 개선점 추가

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,18 @@ import { Helmet } from "react-helmet-async";
 
 function App() {
   return (
-    <div className="App">
+    <div
+      className="App"
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100vh",
+        color: "green",
+        fontSize: 30,
+      }}
+    >
       <Helmet>
         <title>sumin practice</title>
         <meta name="description" content="프론트엔드 과제 사이트" />
@@ -24,18 +35,9 @@ function App() {
         <meta name="twitter:image" content="/public/image/IMG_5916.jpg" />
         <meta name="twitter:card" content="summary_large_image" />
       </Helmet>
-      <h1
-        style={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          height: "100vh",
-          color: "green",
-          fontSize: 50,
-        }}
-      >
-        김수민 과제 입니다!
-      </h1>
+
+      <h1>프론트엔드 개발자 과제 사이트</h1>
+      <h2>프론트엔드 개발자 sumin 과제 입니다!</h2>
     </div>
   );
 }
